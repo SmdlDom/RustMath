394ae5adecd1259da7e90d41eff38757f865fd2d
SmdlDom <DominikBlad@gmail.com>
2023-03-10 15:23:13 -0500

    Implment Crout's LU and row echelon form

--- src/linear_algebra/float_mat.rs
+++ src/linear_algebra/float_mat.rs
@@ -111,6 +111,83 @@ impl<const N: usize, const M: usize> FloatMat<N,M> {
     }
 
     //#endregion Row manipulation
+
+    //#region algo on matrix itself
+
+    pub fn row_echelon_form(&self) -> Self {
+        let mut clone = self.clone();
+        let mut pivot = 0;
+
+        for i in 0..N {
+            // Find pivot row
+            let mut max_row = i;
+            for j in i+1..N {
+                if clone[j][pivot].abs() > clone[max_row][pivot].abs() {
+                    max_row = j;
+                }
+            }
+
+            // Swap pivot row with current row
+            if max_row != i {
+                clone.swap_rows(i, max_row);
+            }
+
+            // Scale pivot row to have a leading coefficient of 1
+            let pivot_coeff = clone[i][pivot];
+            if pivot_coeff != 0.0 {
+                for k in 0..M {
+                    clone[i][k] /= pivot_coeff;
+                }
+            }
+
+            // Make all rows below pivot row zero in current column
+            for j in i+1..N {
+                let factor = clone[j][pivot];
+                clone.add_scaled_row(j, i, -factor);
+            }
+
+            pivot += 1;
+            if pivot >= M {
+                break;
+            }
+        }
+        clone
+    }
+
+    pub fn reduced_row_echelon_form(&self) -> Self {
+        let mut rref = self.row_echelon_form();
+        let mut pivot = 0;
+
+        for i in 0..N {
+            // Find pivot row
+            let mut pivot_row = None;
+            for j in pivot..M {
+                if rref[i][j] != 0.0 {
+                    pivot_row = Some(j);
+                    break;
+                }
+            }
+
+            // If there is a pivot in this row
+            if let Some(pivot_col) = pivot_row {
+                // Make all other rows zero in current column
+                for j in 0..N {
+                    if i != j && rref[j][pivot_col] != 0.0 {
+                        let factor = rref[j][pivot_col];
+                        rref.add_scaled_row(j, i, -factor);
+                    }
+                }
+
+                pivot += 1;
+                if pivot >= M {
+                    break;
+                }
+            }
+        }
+        rref
+    }
+
+    //#endregion algo on matrix itself
 }
 
 impl<const N: usize> FloatMat<N,N> {
@@ -128,7 +205,7 @@ impl<const N: usize> FloatMat<N,N> {
        data
     }
 
-    pub fn triangulate(&self) -> Self {
+    pub fn triangulate_gaussian(&self) -> Self {
         let mut clone = self.clone();
         //Gaussian elimination with maximum selection in column
         for i in 0..N {
@@ -160,11 +237,37 @@ impl<const N: usize> FloatMat<N,N> {
         clone
     }
 
+    pub fn lu_decomposition_crout(&self) -> (FloatMat<N, N>, FloatMat<N, N>) {
+        let mut upper = FloatMat::identity();
+        let mut lower = FloatMat::zero();
+
+        for j in 0..N {
+            for i in j..N {
+                let mut sum = 0.0;
+                for k in 0..j {
+                    sum += lower[i][k] * upper[k][j];
+                }
+                lower[i][j] = self[i][j] - sum;
+            }
+
+            for i in j..N {
+                let mut sum = 0.0;
+                for k in 0..j {
+                    sum += lower[j][k] * upper[k][i];
+                }
+                //TODO should handle cases where the det is 0
+                upper[j][i] = (self[j][i] - sum) / lower[j][j];
+            }
+        }
+
+        (lower, upper)
+    }
+
     pub fn det(&self) -> f64 {
         let mut det = if N % 2 == 0 {1.0} else {-1.0};
 
         // Triangulize the matrix
-        let mat_triangulated = self.triangulate();
+        let mat_triangulated = self.triangulate_gaussian();
 
         // Compute the determinant as the product of the diagonal elements
         for i in 0..N {
@@ -280,15 +383,34 @@ mod float_mat_tests {
     }
 
     #[test]
-    fn float_mat_triangulize_test() {
+    fn float_mat_triangulate_by_gaussian_test() {
         let row_a = FloatN::new([1.0,2.0,3.0]);
         let row_b = FloatN::new([3.0,7.0,1.0]);
         let row_c = FloatN::new([5.0,1.0,2.0]);
         let mat_a = FloatMat::new([row_a, row_b, row_c]);
-        let triangulated =  mat_a.triangulate();
+        let triangulated =  mat_a.triangulate_gaussian();
         assert_eq!(triangulated[0][0], 5.0);
         assert_eq!(triangulated[1][1], 6.4);
         assert_eq!(triangulated[2][2], 2.65625);
+    }
+
+    #[test]
+    fn float_mat_lu_decomposition_by_crout_test() {
+        let row_a = FloatN::new([1.0,2.0,3.0]);
+        let row_b = FloatN::new([3.0,7.0,1.0]);
+        let row_c = FloatN::new([5.0,1.0,2.0]);
+        let mat_a = FloatMat::new([row_a, row_b, row_c]);
+        let (lower, upper) =  mat_a.lu_decomposition_crout();
+        assert_eq!(lower[0][0], 1.0);
+        assert_eq!(lower[1][0], 3.0);
+        assert_eq!(lower[1][1], 1.0);
+        assert_eq!(lower[2][0], 5.0);
+        assert_eq!(lower[2][1], -9.0);
+        assert_eq!(lower[2][2], -85.0);
+        assert_eq!(upper[0][0], 1.0);
+        assert_eq!(upper[0][1], 2.0);
+        assert_eq!(upper[0][2], 3.0);
+        assert_eq!(upper[1][2], -8.0);
     }
 
     #[test]
@@ -299,6 +421,18 @@ mod float_mat_tests {
         let mat_a = FloatMat::new([row_a, row_b, row_c]);
         let det = mat_a.det();
         assert_eq!(det, -85.0);
+    }
+
+    #[test]
+    fn float_mat_row_echelon_form_test() {
+        let row_a = FloatN::new([1.0,2.0,3.0,4.0]);
+        let row_b = FloatN::new([3.0,7.0,1.0,2.0]);
+        let row_c = FloatN::new([5.0,1.0,2.0,1.0]);
+        let mat_a = FloatMat::new([row_a, row_b, row_c]);
+        let echelon = mat_a.reduced_row_echelon_form();
+        assert_eq!(format!("{:.2}", echelon[0][3]), "-0.36");
+        assert_eq!(format!("{:.2}", echelon[1][3]), "0.26");
+        assert_eq!(format!("{:.2}", echelon[2][3]), "1.28");
     }
 }
 
